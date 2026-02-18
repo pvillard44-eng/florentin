f1c69c95da30957bedab3b727d411c370634ece9
pvillard44-eng <pvillard44@gmail.com>
2026-02-18 18:34:08 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  base: '/',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  server: {
+    port: 3000,
+    host: true
   }
 });
