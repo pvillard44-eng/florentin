8e1da079a37b0c9be95b7218451020a74506c445
pvillard44-eng <pvillard44@gmail.com>
2026-02-18 18:23:27 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    // Force la résolution des extensions pour éviter les problèmes sur Netlify
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
   },
   server: {
     port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true
   }
 });
